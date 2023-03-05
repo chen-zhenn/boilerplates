ad17d32a21a93a27179cd9fdea9087b0fa461ef1
Carlos Garcia <chenzhen.dev@gmail.com>
2023-03-05 19:33:15 -0300

    Cleaning index file...

--- vanilla-js/code/front-end/src/js/index.ts
+++ vanilla-js/code/front-end/src/js/index.ts
@@ -1,9 +1,1 @@
-import utils from "./utils/index.js";
 
-const { generateIds } = utils();
-const { get: getId } = generateIds();
-
-console.log( "TypeScript index file..." );
-console.log( `Id: ${getId()}` );
-console.log( `Id: ${getId()}` );
-console.log( `Id: ${getId()}` );
