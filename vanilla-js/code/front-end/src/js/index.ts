1c29b200c39232ff69dddf17a47e1fb1f1e9c414
Carlos Garcia <chenzhen.dev@gmail.com>
2023-03-06 08:39:35 -0300

    Settings proxying data...

--- vanilla-js/code/front-end/src/js/index.ts
+++ vanilla-js/code/front-end/src/js/index.ts
@@ -2,32 +2,37 @@ import utils from "./utils/index";
 import Category from "./Category";
 import CategoryList from "./CategoryList";
 
+type CatItems = ReadonlySet<string>;
+
 const { increment } = utils;
 const random = ( min:number, max:number ) => Math.floor( Math.random() * max ) + min;
 const itemId = increment.id();
 const listId = increment.id();
 
-const catItems:string[] = [ "Metas", "Estudos", "Saúde", "Eventos" ];
+const catItems:CatItems = new Set([ "Metas", "Estudos", "Saúde", "Eventos" ]);
 const catList = new CategoryList(
     random( listId(), 11 ),
-    catItems.map( cat => new Category( random( itemId(), 101 ), cat )));
+    [ ...catItems ].map( cat => new Category( random( itemId(), 101 ), cat )));
 
 function getProxyHandler( callback:( op: string | any ) => string ) {
     return {
         get( target: string[], prop: string | any, receiver:any ) {
-            console.log( `Ojeto alvo foi acessado com prop => ${prop}` );
-            console.log( "Receiver => ", receiver );
-            console.log( "Target => ", target );
+
+            if ( prop === "push" ) {
+                // Fazer algo, antes da execução do push...
+                console.log( callback( prop ));
+            }
 
             return target[ prop ];
         },
         set( target: string[], prop: string | any, value:string, receiver: any ):boolean {
-            console.log( `Ojeto alvo foi alterado com prop => ${prop}` );
-            console.log( "Receiver => ", receiver );
-            console.log( "Target => ", target );
 
-            console.log( callback( prop ));
+            if ( target.length > [ ...catItems ].length ) {
+                // Comparar elementos de target em catItems e segue o baile...
+                console.log( callback( prop ));
+            }
 
+            target[ prop ] = value;
             return true;
         }
     };
@@ -35,8 +40,9 @@ function getProxyHandler( callback:( op: string | any ) => string ) {
 
 const callbackProxy = ( op: string | any ) => `Faça algo, para op => ${op}...`;
 const catItemsHandler = getProxyHandler( callbackProxy );
-const proxy = new Proxy( catItems, catItemsHandler );
+const proxy = new Proxy([ ...catItems ], catItemsHandler );
 
 console.log( catList );
 proxy.push( "Rotina" );
+console.log([ ...catItems ]);
 
